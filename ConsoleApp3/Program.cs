5ed74c700a4cfbdffc01d519349f1b1e2ee46ef9
Matt Eason <capfaneason@gmail.com>
2018-04-15 15:53:12 -0400

    test b

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!w");
+            Console.WriteLine("Hello World!testb");
             Console.ReadLine();
         }
     }
